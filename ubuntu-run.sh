f9835bd39b556ec9c2ae997f36dc2e4616958ed6
FraTeG <fra6217@gmail.com>
2021-02-15 10:15:56 +0100

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,1 +1,2 @@
-echo this can run commands in terminal tho
+echo this can run commands in terminal tho && git config --global user.name "FraTeG" && git config --global user.email "fra6217@gmail.com" && git config --global merge.renamelimit 99999 && git config --global merge.renameLimit 99999
+
